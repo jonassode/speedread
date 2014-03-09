df3cace268ab9c72b427a280354e5c0310173af0
Jonas Soderstrom <jonas.soderstrom@gmail.com>
2014-03-09 21:45:18 +1100

    Support for punctuation.

--- lib.js
+++ lib.js
@@ -1,6 +1,8 @@
 var ar;
 var counter = 0;
 var interval;
+var entry;
+var pause = 0;
 
 function start() {    
     interval = setInterval(updater, 200);
@@ -10,18 +12,31 @@ function updater(){
 
     if ( counter == 0 ){
         t = document.getElementById('t');
-        ar = t.value.split(/[ ,\n]+/);
+        ar = t.value.split(/[ \n]+/);
     }
+
+    if ( pause == 0 ){
     
-    entry = ar[counter]
-    counter = counter + 1;
-    
-    if ( entry != '' && entry != '\n' && entry != undefined  ){
+        entry = ar[counter]
+        counter = counter + 1;
+        
+        
+        if ( entry != '' && entry != '\n' && entry != undefined  ){
+            var first_letter = entry.slice(0)
+            var last_letter = entry.slice(-1)
+            if ( last_letter == "." || last_letter == "," || last_letter == "?" || last_letter == "!" || last_letter == "\"" || first_letter == "\"" ){
+                pause = 1;
+            }
+
             updateword(entry);
-    }
-    
-    if ( counter >= ar.length ){
-        stop();  
+        }
+        
+        if ( counter >= ar.length ){
+            stop();  
+        }
+        
+    } else {
+        pause = 0;
     }
     
 }
